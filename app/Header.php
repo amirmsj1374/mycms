5b658d76c6b2d4fc7a8474928960d2aff6e10f4b
Amir.msj <amir.msj74@gmail.com>
2020-03-29 19:48:15 +0430

    Header Relations

--- app/Header.php
+++ app/Header.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Header extends Model
 {
-    //
+    public function photos () {
+        //hasMany, hasOne, belongsTo
+        return $this->hasMany(HeaderMobilePhoto::class);
+    }
 }
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,21 +13,15 @@ style="    background: #000000 url('{{$header->bg_path}}') no-repeat scroll cent
                 
                 <div class="hidden-xs hidden-sm col-md-5 text-right">
                     <div class="screen-box screen-slider">
-                        <div class="item">
-                            <img src="images/screen-1.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="images/screen-2.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="images/screen-3.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="images/screen-4.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img src="images/screen-5.jpg" alt="">
-                        </div>
+
+                        @foreach ($header->photos as $photo)
+                            
+                            <div class="item">
+                                <img src="{{$photo->path}}">
+                            </div>
+
+                        @endforeach
+                        
                     </div>
                 </div>
 
