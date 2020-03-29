02fc4061620a78a7436cc175109054a9624cca99
Amir.msj <amir.msj74@gmail.com>
2020-03-29 04:49:38 +0430

    Admin Panel Site Map

--- resources/views/dashboards/admin.blade.php
+++ resources/views/dashboards/admin.blade.php
@@ -1,23 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <h2 class="dinar text-center text-primary"> نقشه سایت </h2>
+    <hr>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="text-center p-3">
 
-                    This Is Admin Panel
-                </div>
+        <div class="card my-2">
+            <div class="card-body">
+                <span class="lead"> مدیریت منو </span>
+            </div>
+            <div class="card-footer text-left">
+                <a href="" class="text-decoration-none"> <i class="ti-pencil s-1-5x mx-1 text-success" title="ویرایش"></i> </a>
+                @if ( true )
+                    <a href="" class="text-decoration-none"> <i class="ti-na s-1-5x mx-1 text-warning" title="عدم نمایش"></i> </a>         
+                @else
+                    <a href="" class="text-decoration-none"> <i class="ti-eye s-1-5x mx-1 text-warning" title="نمایش"></i> </a>    
+                @endif
             </div>
         </div>
+        
+
+        <div class="card my-2">
+            <div class="card-body">
+                <span class="lead"> مدیریت هدر </span>
+            </div>
+            <div class="card-footer text-left">
+                <a href="" class="text-decoration-none"> <i class="ti-pencil s-1-5x mx-1 text-success" title="ویرایش"></i> </a>
+                @if ( true )
+                    <a href="" class="text-decoration-none"> <i class="ti-na s-1-5x mx-1 text-warning" title="عدم نمایش"></i> </a>         
+                @else
+                    <a href="" class="text-decoration-none"> <i class="ti-eye s-1-5x mx-1 text-warning" title="نمایش"></i> </a>    
+                @endif
+            </div>
+        </div>
+
+        <div class="card my-2">
+            <div class="card-body">
+                <span class="lead"> مدیریت فوتر</span>
+            </div>
+            <div class="card-footer text-left">
+                <a href="" class="text-decoration-none"> <i class="ti-pencil s-1-5x mx-1 text-success" title="ویرایش"></i> </a>
+                @if ( true )
+                    <a href="" class="text-decoration-none"> <i class="ti-na s-1-5x mx-1 text-warning" title="عدم نمایش"></i> </a>         
+                @else
+                    <a href="" class="text-decoration-none"> <i class="ti-eye s-1-5x mx-1 text-warning" title="نمایش"></i> </a>    
+                @endif
+            </div>
+        </div>
+
     </div>
-</div>
+
 @endsection
