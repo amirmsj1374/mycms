e3bbc4c8379528085c6339b603058120c6de1842
Amir.msj <amir.msj74@gmail.com>
2020-04-02 05:01:01 +0430

    upload file

--- app/Http/Controllers/HeaderController.php
+++ app/Http/Controllers/HeaderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Header;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class HeaderController extends Controller
 {
@@ -27,7 +28,21 @@ class HeaderController extends Controller
             "description" => "nullable|string|max:1000",
             "mobile_visible" => "nullable|boolean",
             "preloader" => "nullable|boolean",
+            "bg_path" => "nullable|mimes:png,jpg,jpeg,bmp,tiff|max:2000",
         ]);
+
+        if ($photo = $request->bg_path) {
+
+            if (file_exists($header->bg_path)) {
+                File::delete($header->bg_path);
+            }
+
+            $file_name = HelperController::rs() . '.' . $photo->getClientOriginalExtension();
+            $relative_path = "storage\app\public" ;
+            $result = $photo->move(base_path($relative_path), $file_name);
+            $data['bg_path'] = "storage/" . $file_name;
+
+        }
         
         $header->update($data);
 
--- resources/views/headers/edit.blade.php
+++ resources/views/headers/edit.blade.php
@@ -2,26 +2,31 @@
 
 @section('content')
 
-    <form class="container" action="{{url("/headers/$header->id")}}" method="post">
+    <form class="container" action="{{url("/headers/$header->id")}}" method="post" enctype="multipart/form-data">
     
         @csrf
         {{ method_field('PUT') }}
 
         <div class="row">
 
-            <div class="col-md-4 my-2">
+            <div class="col-md-3 my-2">
                 <label for="title"> عنوان هدر </label>
                 <input type="text" class="title form-control" name="title" id="title" value="{{$header->title}}">
             </div>
             
-            <div class="col-md-4 my-2">
+            <div class="col-md-3 my-2">
                 <label for="btn-name"> نام دکمه </label>
                 <input type="text" class="btn-name form-control" name="btn_name" id="btn-name" value="{{$header->btn_name}}">
             </div>
             
-            <div class="col-md-4 my-2">
+            <div class="col-md-3 my-2">
                 <label for="btn-link"> لینک دکمه </label>
                 <input type="text" class="btn-link form-control" name="btn_link" id="btn-link" value="{{$header->btn_link}}">
+            </div>
+
+            <div class="col-md-3 my-2">
+                <label for="background"> تصویر پس زمینه </label>
+                <input type="file" class="background form-control" name="bg_path" id="background">
             </div>
 
             
