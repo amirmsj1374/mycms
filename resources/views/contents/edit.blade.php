19b4a28e2d3bd394fc5eb1ba20ec731ac9246f7c
Amir.msj <amir.msj74@gmail.com>
2020-04-07 22:58:54 +0430

    cloning edit content rows

--- resources/views/contents/edit.blade.php
+++ resources/views/contents/edit.blade.php
@@ -7,31 +7,32 @@
         @csrf
         {{method_field("PUT")}}
 
-        <div class="row">
+        <div id="clone-box">
+            <div class="row clone-row">
 
-            <div class="form-group col-md-3 my-2">
-                <label for="position"> ترتیب </label>
-                <input type="number" class="form-control" name="position[]" id="position" value="">
+                <div class="form-group col-md-3 my-2">
+                    <label for="position"> ترتیب </label>
+                    <input type="number" class="form-control" name="position[]" id="position" value="">
+                </div>
+    
+                @foreach ($section->inputs() as $input)
+                    @include("contents.partials.$input")
+                @endforeach
+                
+                <hr class="col-md-11 my-4">
+                
             </div>
-
-            @foreach ($section->inputs() as $input)
-                @include("contents.partials.$input")
-            @endforeach
-            
         </div>
 
-        <hr>
-        
-    </form>
-    
         <div class="row add-row bg-secondary">
-            <div class="col-md-2 mr-auto">
-                <button class="btn btn-success btn-block text-light"> <i class="fa fa-plus ml-1"></i> مورد جدید </button>
+            <div class="col-md-2 my-2 mr-auto">
+                <a class="btn btn-success btn-block text-light" id="cloner"> <i class="fa fa-plus ml-1"></i> مورد جدید </a>
             </div>
 
-            <div class="col-md-2 ml-auto">
+            <div class="col-md-2 my-2 ml-auto">
                 <button class="btn btn-primary btn-block" type="submit"> <i class="fa fa-check ml-1"></i> تایید </button>
             </div>
         </div>
-
+    </form>
+    
     @endsection
--- resources/views/contents/partials/description.blade.php
+++ resources/views/contents/partials/description.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group col-md-12 my-2">
     <label for="description"> توضیحات </label>
-    <textarea class="form-control" name="description" id="description" rows="2"></textarea>
+    <textarea class="form-control" name="description[]" id="description" rows="2"></textarea>
 </div>
