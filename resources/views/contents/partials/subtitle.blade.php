57a6af4f1e5e42e6923485367f2333dea6450795
Amir.msj <amir.msj74@gmail.com>
2020-04-09 08:36:29 +0430

    Dynamic Tabs

--- resources/views/contents/partials/subtitle.blade.php
+++ resources/views/contents/partials/subtitle.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group col-md-3 my-2">
     <label for="subtitle"> زیر عنوان </label>
-    <input type="number" class="form-control" name="subtitle[]" id="subtitle" value="{{$content->subtitle}}">
+    <input type="text" class="form-control" name="subtitle[]" id="subtitle" value="{{$content->subtitle}}">
 </div>
