b8354ad2563f831f172a210fc5c11e5f44ddb6c4
Amir.msj <amir.msj74@gmail.com>
2020-04-12 19:12:05 +0430

    Working With Menu Links

--- resources/views/menu/edit.blade.php
+++ resources/views/menu/edit.blade.php
@@ -13,12 +13,12 @@
 
                 @include('fragments.clone_trash' , ['row_counts' => count($items)])
                 
-                <div class="form-group col-md-2 my-2">
+                <div class="form-group col-md-1 my-2">
                     <label for="position"> ترتیب </label>
                 <input type="number" class="form-control" name="position[]" id="position" value="{{$item->position}}" required>
                 </div>
 
-                <div class="form-group col-md-3 my-2">
+                <div class="form-group col-md-2 my-2">
                     <label for="name"> نام </label>
                     <input type="text" class="form-control" name="name[]" id="name" value="{{$item->name}}">
                 </div>
@@ -29,6 +29,25 @@
                     </label>
                     <input type="text" class="form-control" name="icon[]" id="icon" value="{{$item->icon}}">
                 </div>
+
+                <div class="form-group col-md-3 my-2">
+                    <label for="external_link"> لینک خارجی </label>
+                    <input type="text" class="form-control" name="external_link[]" id="external_link" value="@unless($item->link[0] == '#') {{$item->link}} @endunless">
+                </div>
+
+                <div class="form-group col-md-2 my-2">
+                    <label for="internal_link"> لینک داخلی </label>
+                        <select class="form-control" name="internal_link[]" id="internal_link">
+                            <option value=""> -- انتخاب کنید -- </option>
+                            @foreach ($sections as $section)
+                                <option value="{{$section->id}}" @if($item->section_id() == $section->id) selected  @endif>
+                                    {{$section->position}}- 
+                                    {{translate_section_types($section->type)}} ({{$section->title}})
+                                </option>
+                            @endforeach
+                        </select>
+                </div>
+
             </div>
         @endforeach
     </div>
